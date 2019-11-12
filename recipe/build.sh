090457586822275b61272e8245a9e6ca141592cb
Aaron Meurer <asmeurer@gmail.com>
2019-11-12 00:06:06 -0700

    Make pkg-config a dependency

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,7 +1,7 @@
 mkdir build
 cd build
 cmake -DCMAKE_INSTALL_PREFIX:PATH="$PREFIX" \
-      -DCMAKE_PREFIX_PATH:PATH="$PREFIX"  \
+      -DCMAKE_PREFIX_PATH:PATH="$PREFIX" \
       -DCMAKE_BUILD_TYPE:STRING=Release \
       ..
 make
