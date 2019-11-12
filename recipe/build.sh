8408534860dfc3f65dc118f2f4e667dbaf57536b
Aaron Meurer <asmeurer@gmail.com>
2019-11-11 23:52:04 -0700

    Use cmake

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,4 +1,7 @@
-./autogen.sh
-./configure --prefix="$PREFIX"
+mkdir build
+cd build
+cmake -DCMAKE_INSTALL_PREFIX:PATH="$PREFIX" \
+      -DCMAKE_PREFIX_PATH:PATH="$PREFIX"  \
+      -DCMAKE_BUILD_TYPE:STRING=Release \
+      ..
 make
-make install
